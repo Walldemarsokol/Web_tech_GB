74076d1d7f3f4d778d28493b4147c907af8fa619
walldemarsokol <walldemarsokol@yandex.ru>
2022-12-09 14:07:26 +0300

    add folder seminar3

---
+++ Seminar3/indmain.js
@@ -0,0 +1,17 @@
+// Задача 3: Написать функцию, которая принимает 
+// имя пользователя при ее вызове и выводит 
+// сообщение с приветствием пользователя по имени.
+// Проверить работоспособность функции.
+
+function printname() {
+
+    alert(`Привет, ${username}`);
+}
+let username = prompt('Введите имя');
+printname();
+
+// function sayHello(userName) {
+//     console.log(`Привет, ${userName}`);
+// }
+// const userName = prompt("Введите имя");
+// sayHello(userName);
